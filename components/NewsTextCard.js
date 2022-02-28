96013afd794d7f2186944793b39d29067b943e31
Min Myat Oo <myatviz@gmail.com>
2022-03-01 01:34:55 +0700

    Update Final

--- components/NewsTextCard.js
+++ components/NewsTextCard.js
@@ -1,8 +1,12 @@
 import {Button, Card, Col, Divider, Row, Text} from "@nextui-org/react";
+import Link from "next/link";
 const NewsTextCard = ({item}) => {
+    let uri = item.uri.split('/');
+    let uriValue = uri[3];
     //console.log(article)
     return (
         <>
+            <Link href="/article/[id]" as={`/article/${uriValue}`}>
             <Card hoverable clickable>
                 <Card.Header css={{position: 'absolute', zIndex: 1, top: 5}}>
                     <Col>
@@ -10,7 +14,7 @@ const NewsTextCard = ({item}) => {
                             size={12}
                             weight="bold"
                             transform="uppercase"
-                            color="yellow"
+                            color="red"
                         >
                             MOST VIEWED
                         </Text>
@@ -23,20 +27,21 @@ const NewsTextCard = ({item}) => {
                 <Card.Body css={{p: 2, m: 2}}>
                     <br/>
                     <Divider/>
-                    <Row wrap='wrap' justify="space-between">
-                        <Text size={15} css={{p: 2}}>
+                    <Row wrap='wrap' justify="flex-start">
+                        <Text size={15} css={{p: 20}}>
                             {item.abstract}
                         </Text>
                     </Row>
                 </Card.Body>
                 <Divider/>
-                <Card.Footer justify="flex-start">
+                <Card.Footer justify="space-between">
                     <Row justify="flex-end">
                         <Button size="sm" light>#{item.section}</Button>
-                        <Button size="sm" color="secondary">Read</Button>
+
                     </Row>
                 </Card.Footer>
             </Card>
+            </Link>
         </>
     );
 }
